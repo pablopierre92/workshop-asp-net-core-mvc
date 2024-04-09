fe638f9d1c838a946189dc504be8fbd18d826d09
pablopierre92 <pablo_chips@hotmail.com>
2024-04-09 16:47:55 +0100

    Other entities and second migration

---
+++ SalesWebMvc/SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int   //status de venda
+    {
+        Pending = 0,  //pendende
+        Billed = 1,   //faturado
+        Canceled = 2   //cancelado
+    }
+}
